1ff8326448a35f01bf33a51d07cd7b6bc10fb7bd
davidm07 <0794david@gmail.com>
2024-10-04 20:38:03 -0300

    get, put, delete by id add

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.getFilmes),
+    path('<int:pk>/', views.FilmeById),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -19,3 +19,23 @@ def getFilmes(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def FilmeById(request, pk):
+    try:
+        filme = Filme.objects.get(pk=pk)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = serializers.FilmeSerializer(filme)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'PUT':
+        serializer = serializers.FilmeSerializer(filme, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        filme.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
