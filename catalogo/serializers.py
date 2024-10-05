3d22114f6ff51bc02ad80d2d9497825fb9fb3c26
davidm07 <0794david@gmail.com>
2024-10-04 21:42:45 -0300

    validation by year create

--- catalogo/serializers.py
+++ catalogo/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from datetime import datetime
 from .models import Filme
 
 
@@ -6,3 +7,9 @@ class FilmeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Filme
         fields = '__all__'
+
+    def validate_ano(self, value):
+        ano_atual = datetime.now().year
+        if value < 1900 or value > ano_atual:
+            raise serializers.ValidationError(f"O ano deve estar entre 1900 e {ano_atual}.")
+        return value
